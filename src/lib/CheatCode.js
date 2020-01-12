0485a8c3365b8d7c0419defcb1bd7ec592a9c57f
David <david-d25@yandex.ru>
2020-01-12 21:50:15 +0300

    Implemented cheats

---
+++ src/lib/CheatCode.js
@@ -0,0 +1,29 @@
+export default function () {
+  let map = {};
+  let maxBufferLength = 0;
+  let buffer = '';
+
+  this.addCheatListener = function (code, handler) {
+    map[code.toUpperCase()] = handler;
+
+    for (let it in map)
+      if (it.length > maxBufferLength)
+        maxBufferLength = it.length;
+  };
+
+  window.addEventListener('keypress', onKeyPress);
+
+  function onKeyPress(event) {
+    if (event.key.length === 1) { // It's a character key
+      buffer += event.key.toUpperCase();
+      if (buffer.length > maxBufferLength)
+        buffer = buffer.substring(1);
+
+      for (let it in map) {
+        if (it.endsWith(it)) {
+          map[it]();
+        }
+      }
+    }
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CheatCode from '$src/lib/CheatCode'
+
+window.cheatCode = new CheatCode();
 
 new Vue({
   el: '#app',
